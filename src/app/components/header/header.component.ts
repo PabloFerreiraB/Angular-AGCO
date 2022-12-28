ac44bdc44809abf9b76af8b1349ddc63b38d2f0e
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2022-12-28 19:31:25 -0300

    refactor: new changes to component select

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonComponent } from '../button/button.component';
 import { SelectComponent } from '../select/select.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -21,9 +22,37 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   @Input() user!: string;
   @Output() onLogin = new EventEmitter<Event>();
   @Output() onLogout = new EventEmitter<Event>();
   @Output() onCreateAccount = new EventEmitter<Event>();
+
+  form!: FormGroup;
+  countriesList: any = [
+    { id: 'steak-0', name: 'Steak' },
+    { id: 'pizza-1', name: 'Pizza' },
+    { id: 'tacos-2', name: 'Tacos' },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initialForm();
+  }
+
+  waitForFilterResponse(value: string) {
+    this.getCountryList({ name: value });
+  }
+
+  private getCountryList(params: object = {}): void {
+    // params will be converted in url query params ?name={{value}}
+    // this.countriesList = this.geoService.countryList(params);
+  }
+
+  private initialForm(): void {
+    this.form = this.fb.group({
+      dealer: [''],
+    });
+  }
 }
--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Injector,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { map, merge, Observable, of } from 'rxjs';
+import { debounceTime, map, merge, Observable, of, startWith } from 'rxjs';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
-import { OPTIONS } from './options';
+import { FormControlValueAccessorConnector } from './control-value-accessor/control-value-accessor.component';
 
 @Component({
   selector: 'app-select',
@@ -25,34 +39,79 @@ import { OPTIONS } from './options';
   ],
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SelectComponent,
+      multi: true,
+    },
+  ],
 })
-export class SelectComponent implements OnInit {
-  singleSelectedWithSearch!: string;
+export class SelectComponent
+  extends FormControlValueAccessorConnector
+  implements OnInit, OnChanges
+{
+  @Input() appearance!: 'legacy' | 'standard' | 'fill' | 'outline';
+  @Input() placeholder: string = "Specify placeholder='you placeholder here'";
+  @Input() items: Observable<Array<any>> | null | undefined;
 
-  searchCtrl: FormControl = new FormControl();
-  filteredOptions?: Observable<string[]>;
+  @Input() bindValueKey: string = 'value';
+  @Input() bindLabelKey: string = 'label';
+  @Input() searchPlaceholder: string = 'Search your item ...';
+  @Output() itemFilterServerSide = new EventEmitter<string>();
+  filterFormControl: FormControl = new FormControl('');
 
-  private options: string[] = OPTIONS;
+  private isServerSide: boolean = true;
+  private currentStaticItems: Array<any> = [];
 
-  ngOnInit(): void {
-    this.filteredOptions = merge(
-      of(OPTIONS),
-      this.searchCtrl.valueChanges.pipe(
-        map((value: any) => {
-          console.log(this.getFilteredOptions(value));
-          return this.getFilteredOptions(value);
-        })
-      )
+  constructor(injector: Injector) {
+    super(injector);
+  }
+
+  private listenToFilterFormControlChanges(): void {
+    this.filterFormControl.valueChanges
+      .pipe(startWith(''), debounceTime(300))
+      .subscribe((value: string) => {
+        if (this.isServerSide) {
+          this.itemFilterServerSide.emit(value);
+        } else {
+          this.filterStaticList(value);
+        }
+      });
+  }
+
+  private filterStaticList(value: string) {
+    const currentItems = this.currentStaticItems;
+    console.log('1', currentItems);
+
+    const filterValue = this._normalizeValue(value);
+    console.log('2', filterValue);
+
+    this.items = of(
+      currentItems.filter((item) => {
+        console.log('3', item);
+
+        return this._normalizeValue(item).includes(filterValue);
+      })
     );
   }
 
-  private getFilteredOptions(value: any): string[] {
-    const searchFilter = value && value.new ? value.value : value;
+  private _normalizeValue(value: any): string {
+    if (typeof value != 'string') {
+      value = value[this.bindLabelKey];
+    }
+    return value?.toLowerCase().replace(/\s/g, '');
+  }
 
-    return searchFilter
-      ? this.options.filter((option) =>
-          option.toLowerCase().includes(searchFilter.toLowerCase())
-        )
-      : this.options;
+  ngOnInit(): void {
+    this.listenToFilterFormControlChanges();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.items instanceof Array) {
+      this.currentStaticItems = this.items;
+      this.items = of(this.items);
+      this.isServerSide = false;
+    }
   }
 }
